838bb32a6e0643ab9b00ab19d700c8539e2f3a7c
viswam007 <viswam.i@gmail.com>
2020-10-07 19:24:23 -0400

    Commiting

---
+++ HelloWorld_Vis/src/test/java/HelloWorld_Vis/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld_Vis;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void TestHelloWorld() {
+		System.out.println("....HelloWorld..Viswanath..");
+		
+
+}
+	
+}
